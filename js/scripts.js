ff826e63fd6044689c08223757005bb220db48d3
dojo24 <mlongenberger@codingdojo.com>
2021-03-13 17:07:43 -0500

    added some js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    document.getElementById("color").style.color = "green"
+    console.log("changes font color")
+}
+
+function hideImage() {
+    document.getElementById("hide").style.display = "none"
+    console.log("hid image")
+}
